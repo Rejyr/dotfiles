812b912082cc173c6cebbcd46252bbc7db9a6696
Rejyr <jerrylwang123@gmail.com>
2022-05-05 06:01:52 -0400

    change: hardcode weather.sh location

--- .config/waybar/weather.sh
+++ .config/waybar/weather.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 # Updates weather forecast
 
-ip r | while read Def _ IP _; do
-	case $Def in
-		default)
-			ping -q -w 1 -c 1 "$IP" >/dev/null || exit
-
-			break ;;
-	esac
-done
-
-geoip=$(curl -s https://freegeoip.app/csv/)
-country_code=$(echo $geoip | cut -d, -f 2)
-location=$(echo $geoip | cut -d, -f 6)
+country_code="US"
+location="Allentown"
 ansiweather -l $location,$country_code -u metric -s true -f 1 -a false | cut -d' ' -f7,8-
