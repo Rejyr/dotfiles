80277c3633fd141b78b23151718c9a3fe128756f
Rejyr <jerrylwang123@gmail.com>
2022-10-21 16:28:22 -0400

    change: switch to `noice.nvim` in `nvim`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -61,30 +61,60 @@ return require('packer').startup(function(use)
     --
     --
 
-    -- ui select
-    use 'stevearc/dressing.nvim'
+    -- pretty stuff
+    use {
+        'folke/noice.nvim',
+        event = 'VimEnter',
+        config = function()
+            require('noice').setup {
+                views = {
+                    cmdline_popup = {
+                        position = {
+                            row = 15,
+                            col = '50%',
+                        },
+                        size = {
+                            width = 80,
+                            height = 'auto',
+                        },
+                    },
+                    popupmenu = {
+                        relative = 'editor',
+                        position = {
+                            row = 18,
+                            col = '51%',
+                        },
+                        size = {
+                            width = 82,
+                            height = 10,
+                        },
+                        border = {
+                            style = 'rounded',
+                            padding = { 0, 1 },
+                        },
+                        win_options = {
+                            winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+                        },
+                    },
+                },
+            }
+            require('telescope').load_extension 'noice'
+        end,
+        requires = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            'MunifTanjim/nui.nvim',
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            'rcarriga/nvim-notify',
+        },
+    }
 
     -- highlight colors
     use {
         'norcalli/nvim-colorizer.lua',
         config = function()
             require('colorizer').setup()
-        end,
-    }
-
-    -- prettier notifications
-    use {
-        'rcarriga/nvim-notify',
-        config = function()
-            vim.notify = require 'notify'
-        end,
-    }
-
-    -- lsp status
-    use {
-        'j-hui/fidget.nvim',
-        config = function()
-            require('fidget').setup {}
         end,
     }
 
@@ -102,81 +132,6 @@ return require('packer').startup(function(use)
                 dynamicRegistration = false,
                 lineFoldingOnly = true,
             }
-        end,
-    }
-
-    -- fancy wildmenu
-    use {
-        'gelguy/wilder.nvim',
-        config = function()
-            local wilder = require 'wilder'
-            wilder.setup { modes = { ':', '/', '?' } }
-
-            -- fuzzy finding
-            wilder.set_option('pipeline', {
-                wilder.branch(
-                    wilder.cmdline_pipeline {
-                        -- sets the language to use, 'vim' and 'python' are supported
-                        language = 'python',
-                        -- 0 turns off fuzzy matching
-                        -- 1 turns on fuzzy matching
-                        -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
-                        fuzzy = 1,
-                    },
-                    wilder.python_search_pipeline {
-                        -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
-                        pattern = wilder.python_fuzzy_pattern(),
-                        -- omit to get results in the order they appear in the buffer
-                        sorter = wilder.python_difflib_sorter(),
-                        -- can be set to 're2' for performance, requires pyre2 to be installed
-                        -- see :h wilder#python_search() for more details
-                        engine = 're',
-                    }
-                ),
-            })
-
-            -- file filnding
-            wilder.set_option('pipeline', {
-                wilder.branch(
-                    wilder.python_file_finder_pipeline {
-                        -- to use ripgrep : {'rg', '--files'}
-                        -- to use fd      : {'fd', '-tf'}
-                        file_command = { 'find', '.', '-type', 'f', '-printf', '%P\n' },
-                        -- to use fd      : {'fd', '-td'}
-                        dir_command = { 'find', '.', '-type', 'd', '-printf', '%P\n' },
-                        -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
-                        -- found at https://github.com/nixprime/cpsm
-                        filters = { 'fuzzy_filter', 'difflib_sorter' },
-                    },
-                    wilder.cmdline_pipeline(),
-                    wilder.python_search_pipeline()
-                ),
-            })
-
-            -- fancy popmenu
-            wilder.set_option(
-                'renderer',
-                wilder.popupmenu_renderer(wilder.popupmenu_border_theme {
-                    highlights = {
-                        border = 'Normal', -- highlight to use for the border
-                    },
-                    -- 'single', 'double', 'rounded' or 'solid'
-                    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-                    border = 'rounded',
-                    left = {
-                        ' ',
-                        wilder.popupmenu_devicons(),
-                        wilder.popupmenu_buffer_flags {
-                            flags = ' a + ',
-                            icons = { ['+'] = '', a = '', h = '' },
-                        },
-                    },
-                    right = {
-                        ' ',
-                        wilder.popupmenu_scrollbar(),
-                    },
-                })
-            )
         end,
     }
 
