3fd8c62fb4dfe66291e6c65c2d41a21bef02cd3f
Rejyr <jerrylwang123@gmail.com>
2022-11-06 11:53:36 -0500

    add: `rust.vimrc.lua`

---
+++ .config/nvim/lua/rust.vimrc.lua
@@ -0,0 +1,22 @@
+require('lspconfig').rust_analyzer.setup {
+    settings = {
+        ['rust-analyzer'] = {
+            checkOnSave = {
+                overrideCommand = { 'python3', 'x.py', 'check', '--json-output' },
+            },
+            rustfmt = { overrideCommand = { './build/x86_64-unknown-linux-gnu/stage0/bin/rustfmt', '--edition=2021' } },
+            procMacro = { server = './build/x86_64-unknown-linux-gnu/stage0/libexec/rust-analyzer-proc-macro-srv', enable = true },
+            -- procMacro = { enable = false },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                    invocationLocation = 'root',
+                    invocationStrategy = 'once',
+                    overrideCommand = { 'x', 'check', '--json-output' },
+                },
+                sysroot = './build/x86_64-unknown-linux-gnu/stage0-sysroot',
+            },
+            rustc = { source = './Cargo.toml' },
+        },
+    },
+}
