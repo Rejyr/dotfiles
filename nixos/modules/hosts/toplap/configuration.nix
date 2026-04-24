3233773302e84fea382e14ee8536d2284a27ab85
Jerry Wang <jerrylwang123@gmail.com>
2026-04-23 22:21:28 -0400

    feat: limine boot loader

--- nixos/modules/hosts/toplap/configuration.nix
+++ nixos/modules/hosts/toplap/configuration.nix
@@ -30,7 +30,30 @@
         self.nixosModules.tzLocale
       ];
 
-      boot.loader.systemd-boot.enable = true;
+      boot.loader.limine = {
+        enable = true;
+        extraEntries = ''
+          /Windows
+              protocol: efi
+              path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
+        '';
+        style = {
+          backdrop = "2D353B";
+          wallpapers = [ ../../../../wallpapers/shenandoah.png ];
+          wallpaperStyle = "centered";
+          interface = {
+            brandingColor = 2;
+          };
+          graphicalTerminal = {
+            palette = "232A2E;E67E80;A7C080;E69875;7FBBB3;D699B6;83C092;7A8478";
+            foreground = "D3C6AA";
+            background = "402D353B";
+            brightPalette = "233A2E;E67E80;A7C080;E69875;7FBBB3;D699B6;83C092;7A8478";
+            brightForeground = "D3C6AA";
+            brightBackground = "402D353B";
+          };
+        };
+      };
       boot.loader.efi.canTouchEfiVariables = true;
 
       networking.hostName = "toplap";
