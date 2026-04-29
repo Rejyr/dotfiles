70c14e5fea6cb2fd0fca0b1fe494133331854332
Jerry Wang <jerrylwang123@gmail.com>
2026-04-29 00:44:22 -0400

    feat(nvim): mini pick send to quickfix

--- nixos/modules/features/wrapperModules/neovim/lua/rejyr/plugins.lua
+++ nixos/modules/features/wrapperModules/neovim/lua/rejyr/plugins.lua
@@ -80,7 +80,6 @@ later(function()
   require('mini.jump').setup()
   require('mini.notify').setup()
   require('mini.pairs').setup()
-  require('mini.pick').setup()
   require('mini.surround').setup()
 end)
 
@@ -129,6 +128,17 @@ later(function()
       n_steps_ahead = 1,
     },
     mappings = { start_jumping = '' },
+  }
+end)
+later(function()
+  local choose_all = function()
+    local mappings = MiniPick.get_picker_opts().mappings
+    vim.api.nvim_input(mappings.mark_all .. mappings.choose_marked)
+  end
+  require('mini.pick').setup {
+    mappings = {
+      choose_all = { char = '<C-q>', func = choose_all },
+    },
   }
 end)
 later(function()
